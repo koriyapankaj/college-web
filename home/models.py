f9f181d7a04ec36236ea8fd461874e7a39dfa27d
pankaj koriya <koriyapankaj007@gmail.com>
2022-06-26 15:15:31 +0530

    gallery model created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Gallery(models.Model):
+
+    g_title=models.CharField(max_length=50)
+    g_disc=models.TextField()
+    g_image=models.ImageField(upload_to='assets/pics')
+
+    def __str__(self):
+        return self.g_title
+    
